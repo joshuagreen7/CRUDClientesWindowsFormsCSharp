77f5fa0308424b8882b0d8e13c7841809eb3867b
Joshua Green <jogra073@gmail.com>
2024-05-03 02:30:52 -0500

    Se agrega Proyecto Windows Forms

---
+++ CRUDJGA/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace CRUDJGA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.cbxPais = new System.Windows.Forms.ComboBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNuevo);
+            this.groupBox1.Controls.Add(this.btnEliminar);
+            this.groupBox1.Controls.Add(this.btnActualizar);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.cbxPais);
+            this.groupBox1.Controls.Add(this.txtTelefono);
+            this.groupBox1.Controls.Add(this.txtDireccion);
+            this.groupBox1.Controls.Add(this.txtNombre);
+            this.groupBox1.Controls.Add(this.txtCodigo);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(28, 47);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 391);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos del Cliente";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(252, 320);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(171, 320);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 5;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(89, 320);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // cbxPais
+            // 
+            this.cbxPais.FormattingEnabled = true;
+            this.cbxPais.Location = new System.Drawing.Point(23, 202);
+            this.cbxPais.Name = "cbxPais";
+            this.cbxPais.Size = new System.Drawing.Size(151, 21);
+            this.cbxPais.TabIndex = 4;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(23, 257);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(141, 20);
+            this.txtTelefono.TabIndex = 3;
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Location = new System.Drawing.Point(23, 150);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(211, 20);
+            this.txtDireccion.TabIndex = 3;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(23, 93);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(162, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(23, 43);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(121, 20);
+            this.txtCodigo.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 241);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Telefono";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Pais";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Direccion";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // dgvClientes
+            // 
+            this.dgvClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClientes.Location = new System.Drawing.Point(381, 13);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.Size = new System.Drawing.Size(518, 425);
+            this.dgvClientes.TabIndex = 1;
+            this.dgvClientes.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvClientes_CellClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(33, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(322, 24);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "MANTENIMIENTO DE CLIENTES";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(9, 320);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 6;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dgvClientes);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbxPais;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvClientes;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnNuevo;
+    }
+}
+
---
+++ CRUDJGA/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace CRUDJGA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Métodos
+        private static string cadena = ConfigurationManager.ConnectionStrings["CadenaSql"].ConnectionString;
+        private SqlConnection cnx = new SqlConnection(cadena);
+        public void ListarPaises()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("usp_Pais_Listar", cnx);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            cbxPais.DataSource = dt;
+            cbxPais.DisplayMember = "NombrePais";
+            cbxPais.ValueMember = "Idpais";
+        }
+        public void ListarClientes()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("usp_Clientes_ListarTodos", cnx);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dgvClientes.DataSource = dt;
+        }
+        public void InsertarClientes(string codigoCliente, string nombreCliente, string direccion, string idPais, string telefono)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("usp_Cliente_Insertar", cnx);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //cmd.Parameters.Add("@IdCliente", SqlDbType.VarChar, 5, codigoCliente);
+                //cmd.Parameters.Add("@NombreCia", SqlDbType.VarChar, 40, nombreCliente);
+                //cmd.Parameters.Add("@Direccion", SqlDbType.VarChar, 60, direccion);
+                //cmd.Parameters.Add("@IdPais", SqlDbType.Char, 3, idPais);
+                //cmd.Parameters.Add("@Telefono", SqlDbType.VarChar, 24, telefono);
+                cmd.Parameters.AddWithValue("IdCliente", codigoCliente);
+                cmd.Parameters.AddWithValue("NombreCia", nombreCliente);
+                cmd.Parameters.AddWithValue("Direccion", direccion);
+                cmd.Parameters.AddWithValue("IdPais",  idPais);
+                cmd.Parameters.AddWithValue("Telefono", telefono);
+
+                cnx.Open();
+
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show($"Se ha agregado {i} cliente");
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally 
+            {
+                cnx.Close();
+            }
+
+        } //fin de insertar clientes...
+        public void ActualizarClientes(string codigoCliente, string nombreCliente, string direccion, string idPais, string telefono)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("usp_Cliente_Actualizar", cnx);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdCliente", codigoCliente);
+                cmd.Parameters.AddWithValue("@NombreCia", nombreCliente);
+                cmd.Parameters.AddWithValue("@Direccion", direccion);
+                cmd.Parameters.AddWithValue("@IdPais", idPais);
+                cmd.Parameters.AddWithValue("@Telefono", telefono);
+
+                cnx.Open();
+
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show($"Se ha actualizado {i} cliente");
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cnx.Close();
+            }
+        }// fin de ActualizarCliente...
+
+        public void EliminarClientes(string codigoCliente)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("usp_Cliente_Eliminar", cnx);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdCliente", codigoCliente);
+             
+                cnx.Open();
+
+                int i = cmd.ExecuteNonQuery();
+                MessageBox.Show($"Se ha eliminado {i} cliente");
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cnx.Close();
+            }
+
+        } //fin de EliminarClientes...
+        void LimpiarFormulario() 
+        {
+            btnAgregar.Enabled = true;
+            btnActualizar.Enabled = false;
+            btnEliminar.Enabled = true;
+
+            txtCodigo.Text = String.Empty;
+            txtNombre.Text = String.Empty;
+            txtDireccion.Text = String.Empty;
+            txtTelefono.Text = String.Empty;
+            
+            txtCodigo.ReadOnly = false;
+            txtCodigo.Focus();
+        } //fin de LimpiarFormulario() ...
+
+
+
+        #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListarPaises();
+            ListarClientes();
+
+            btnActualizar.Enabled = false;
+            btnEliminar.Enabled = false;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            InsertarClientes(txtCodigo.Text, txtNombre.Text, txtDireccion.Text, cbxPais.SelectedValue.ToString(), txtTelefono.Text);
+            ListarClientes() ;
+            LimpiarFormulario();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            ActualizarClientes(txtCodigo.Text, txtNombre.Text, txtDireccion.Text, cbxPais.SelectedValue.ToString(), txtTelefono.Text);
+            ListarClientes();
+            LimpiarFormulario();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            EliminarClientes(txtCodigo.Text);
+            ListarClientes();
+            LimpiarFormulario();
+        }
+
+        private void dgvClientes_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewRow fila = dgvClientes.CurrentRow;
+            txtCodigo.Text = fila.Cells[0].Value.ToString();
+            txtNombre.Text = fila.Cells[1].Value.ToString();
+            txtDireccion.Text = fila.Cells[2].Value.ToString();
+            cbxPais.SelectedValue = fila.Cells[3].Value.ToString();
+            txtTelefono.Text = fila.Cells[4].Value.ToString();
+
+            txtCodigo.ReadOnly = true;
+            btnAgregar.Enabled = false;
+            btnActualizar.Enabled = true;
+            btnEliminar.Enabled = true;
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            LimpiarFormulario();
+            ListarClientes();
+        }
+    }
+}
